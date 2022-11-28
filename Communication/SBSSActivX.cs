152b9c07bcc055d37a587e027e6f9f02f4e3e590
Kévin MARCONI <kmarconi@nbstech.eu.com>
2022-11-28 15:23:37 +0100

    Created Communication and Interface folders

--- Communication/SBSSActivX.cs
+++ Communication/SBSSActivX.cs
@@ -5,14 +5,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PocSBSS
+namespace PocSBSS.Communication
 {
     public class SBSSActivX
     {
-        sbWpnActiveX.IsbWpnActiveXClient sbssActiveX = new sbWpnActiveXClientClass();
-        public void init ()
+        IsbWpnActiveXClient sbssActiveX = new sbWpnActiveXClient();
+        public void init()
         {
-            
+
             sbssActiveX.ClientName = "WpsSecs";
             sbssActiveX.UserName = "root";
             sbssActiveX.Port = 10073;
@@ -24,15 +24,14 @@ namespace PocSBSS
             sbssActiveX.MultiInstance = true;
             sbssActiveX.Cursor = 0;
         }
-        public void connect ()
+        public void connect()
         {
-            
             sbssActiveX.Open();
         }
-        public sbWpnActiveX.IsbWpnActiveXClient get()
+        public IsbWpnActiveXClient get()
         {
             return sbssActiveX;
         }
-        
+
     }
 }
